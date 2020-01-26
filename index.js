d3b46f019905094069e785cdae3147db8437d95a
greatplainsmodding <greatplainsmodding@gmail.com>
2020-01-26 00:18:01 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// project init
+const express = require('express');
+const app = express();
+
+// Load Routes
+const mainRoute = require('./routes/mainRoute');
+
+// App Settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+// Set Routes
+app.use('/', mainRoute);
+
+// Start Express Server
+app.listen(2000, () => console.log('Express Server started on port 2000'));
---
+++ routes/mainRoute.js
@@ -0,0 +1,9 @@
+// project init
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('homePage')
+})
+
+module.exports = router;
